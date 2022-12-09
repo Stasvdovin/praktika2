a162674d4d14768e1118953e18ca3bc7a3ead5b8
Stasvdovin <vdovin.kontent@gmail.com>
2022-12-09 06:08:50 +0500

    praktika2.py

--- praktika2.py
+++ praktika2.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from transformers import pipeline
 classifier = pipeline(model="SkolkovoInstitute/russian_toxicity_classifier")
-st.title("Приложение определения токсичный комментариве на русском языке")
+st.title("Приложение определения токсичный комментариев на русском языке")
 name = st.text_input("Введите текст")
 if(st.button('Отправить')):
     result = classifier(name)
