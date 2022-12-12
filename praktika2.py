a5b24a77d88380c9a58ce84b00667f6488a87012
Stasvdovin <117981863+Stasvdovin@users.noreply.github.com>
2022-12-12 06:02:35 +0500

    Update praktika2.py

--- praktika2.py
+++ praktika2.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from transformers import pipeline
 # Создаем модель для определения комментариев 
-classifier = pipeline(model="SkolkovoInstitute/russian_toxicity_classifier")
+classifier = pipeline(model="Helsinki-NLP/opus-mt-ru-en")
 # Формируем заголовок для браузера
 st.title("Приложение определения токсичный комментариев на русском языке")
 # Строчка для ввода текста 
